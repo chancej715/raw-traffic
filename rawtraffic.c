4ce20211d7fb28afc9ac036ec6a49239982c0442
Chance Johnson <chancej715@gmail.com>
2023-03-18 08:53:05 -0500

    Capture packet with "pcap_dispatch" function and save binary to file.

--- rawtraffic.c
+++ rawtraffic.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <pcap.h>
 
 int main(int argc, char *argv[])
@@ -11,7 +12,10 @@ int main(int argc, char *argv[])
 	bpf_u_int32 mask;			   	// Netmask of capturing device
 	bpf_u_int32 net;			   	// IP of capturing device
 	struct pcap_pkthdr header;		// Header pcap gives us
-	const unsigned char *packet;	// The packet
+
+	pcap_dumper_t *file_pointer;	// Pointer to the dump file
+	char *filename = "packet";		// File to save to
+	int pcount = 0;					// Number of packets read
 
 	printf("Device: %s\n", dev);
 
@@ -39,24 +43,90 @@ int main(int argc, char *argv[])
 	if (pcap_compile(handle, &fp, filter_exp, 0, net) == -1)
 	{
 		fprintf(stderr, "Couldn't parse filter %s: %s\n", filter_exp, pcap_geterr(handle));
-		return (2);
+		return 2;
 	}
 
 	// Set the filter
 	if (pcap_setfilter(handle, &fp) == -1)
 	{
 		fprintf(stderr, "Couldn't install filter %s: %s\n", filter_exp, pcap_geterr(handle));
-		return (2);
+		return 2;
 	}
 
 	/**
-	 * Capture packet
+	 * Capture and save packets
 	*/
-	packet = pcap_next(handle, &header);
+	// Open dump device for writing captured packet data
+	file_pointer = pcap_dump_open(handle, filename);
+	if (file_pointer == NULL)
+	{
+		fprintf(stderr, "Error opening file \"%s\" for writing: %s\n", filename, pcap_geterr(handle));
+		return 2;
+	}
+	
+	// Capture 1 packet and save to file
+	pcount = pcap_dispatch(handle, 1, &pcap_dump, (char *)file_pointer);
+	if (pcount < 0)
+	{
+		fprintf(stderr, "Error reading packets from interface %s", dev);
+		return 2;
+	}
 
-	printf("Packet length: %d\n", header.len);
+	// Close file
+	pcap_dump_close(file_pointer);
 
+	// Close packet capture device
 	pcap_close(handle);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	// /**
+	//  * Capture packet and write to file
+	// */
+	// packet = pcap_next(handle, &header);
+
+	// // Write packet to file
+	// file_pointer = pcap_dump_open(handle, filename);
+	// if (file_pointer == NULL)
+	// {
+	// 	fprintf(stderr, "Couldn't open file \"%s\" for writing: %s\n", filename, pcap_geterr(handle));
+	// 	return 2;
+	// }
+
+	// // Close the file
+	// pcap_dump_close(file_pointer);
+
+	// // Close the capture device
+	// pcap_close(handle);
 
 	return 0;
 }
