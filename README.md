224e6e10e77161b95066dbad372b2614ad8e619e
Chance Johnson <chancej715@gmail.com>
2023-03-18 05:14:58 -0500

    Traffic filter expressions.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Raw Traffic
-This is a simple program for capturing network traffic on an interface and saving it to a binary file using libpcap.
+This is a simple program for capturing IPv4 network traffic on an interface and saving it to a binary file using libpcap.
 
 To make this program, I followed the [Programming With Pcap](https://www.tcpdump.org/pcap.html) guide.
 
@@ -9,4 +9,10 @@ This program requires the libpcap development libraries.
 If you're on Debian, you can install these libraries via the following command: 
 ```
 sudo apt-get install libpcap-dev
+```
+
+## Compiling
+If you're compiling with `gcc`, you must include `-l pcap` to search the `libpcap` library when linking.
+```
+gcc rawtraffic.c -o rawtraffic -lpcap
 ```
--- rawtraffic.c
+++ rawtraffic.c
@@ -1,25 +1,48 @@
 #include <stdio.h>
 #include <pcap.h>
 
-// User must pass device to capture as a command line argument.
+// User must pass device to capture as a command line argument
 int main(int argc, char *argv[])
 {
-	char *dev = argv[1], errbuf[PCAP_ERRBUF_SIZE];
+	pcap_t *handle;				   	// Session handle
+	char *dev = argv[1];		   	// Device to capture on
+	char errbuf[PCAP_ERRBUF_SIZE]; 	// Error string
+	struct bpf_program fp;		   	// Compiled filter expression
+	char filter_exp[] = "port 8000";// Filter expression
+	bpf_u_int32 mask;			   	// Netmask of capturing device
+	bpf_u_int32 net;			   	// IP of capturing device
 
 	printf("Device: %s\n", dev);
-	return (0);
 
-	// Open the device for sniffing.
-	pcap_t *handle;
+	// Find the IPv4 network number and netmask associated with device
+	if (pcap_lookupnet(dev, &net, &mask, errbuf) == -1)
+	{
+		fprintf(stderr, "Can't get netmask for device %s\n", dev);
+		net = 0;
+		mask = 0;
+	}
 
+	// Open the device for capturing
 	handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
 	if (handle == NULL)
 	{
 		fprintf(stderr, "Couldn't open device %s: %s\n", dev, errbuf);
+		return 2;
+	}
+
+	// Compile the filter expression string
+	if (pcap_compile(handle, &fp, filter_exp, 0, net) == -1)
+	{
+		fprintf(stderr, "Couldn't parse filter %s: %s\n", filter_exp, pcap_geterr(handle));
 		return (2);
 	}
 
-
+	// Set the filter
+	if (pcap_setfilter(handle, &fp) == -1)
+	{
+		fprintf(stderr, "Couldn't install filter %s: %s\n", filter_exp, pcap_geterr(handle));
+		return (2);
+	}
 
 	return 0;
 }
