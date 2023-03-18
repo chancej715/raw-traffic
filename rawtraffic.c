45c1b1d7d5ae40607fa079f2f83b4e57d5b7d474
Chance Johnson <chancej715@gmail.com>
2023-03-18 09:03:44 -0500

    Cleaned up code.

--- rawtraffic.c
+++ rawtraffic.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <pcap.h>
 
 int main(int argc, char *argv[])
@@ -11,18 +10,14 @@ int main(int argc, char *argv[])
 	char filter_exp[] = "port 8000";// Filter expression
 	bpf_u_int32 mask;			   	// Netmask of capturing device
 	bpf_u_int32 net;			   	// IP of capturing device
-	struct pcap_pkthdr header;		// Header pcap gives us
-
+	
 	pcap_dumper_t *file_pointer;	// Pointer to the dump file
-	char *filename = "packet";		// File to save to
+	char *filename = "packet";		// Name of file to save to
 	int pcount = 0;					// Number of packets read
-
-	printf("Device: %s\n", dev);
 
 	/**
 	 * Open device and set filter
 	*/
-
 	// Find the IPv4 network number and netmask associated with device
 	if (pcap_lookupnet(dev, &net, &mask, errbuf) == -1)
 	{
@@ -64,7 +59,7 @@ int main(int argc, char *argv[])
 		return 2;
 	}
 	
-	// Capture 1 packet and save to file
+	// Capture packets and save to file
 	pcount = pcap_dispatch(handle, 1, &pcap_dump, (char *)file_pointer);
 	if (pcount < 0)
 	{
@@ -77,56 +72,6 @@ int main(int argc, char *argv[])
 
 	// Close packet capture device
 	pcap_close(handle);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	// /**
-	//  * Capture packet and write to file
-	// */
-	// packet = pcap_next(handle, &header);
-
-	// // Write packet to file
-	// file_pointer = pcap_dump_open(handle, filename);
-	// if (file_pointer == NULL)
-	// {
-	// 	fprintf(stderr, "Couldn't open file \"%s\" for writing: %s\n", filename, pcap_geterr(handle));
-	// 	return 2;
-	// }
-
-	// // Close the file
-	// pcap_dump_close(file_pointer);
-
-	// // Close the capture device
-	// pcap_close(handle);
 
 	return 0;
 }
